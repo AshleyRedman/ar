be496eb432c99da80fd6dc1c5f34905710910a77
Ashley Redman <ashredman@hotmail.com>
2020-04-19 03:16:05 +0100

    Updated Mix to add extract
    Extracted out vendors

--- resources/views/layouts/_head.blade.php
+++ resources/views/layouts/_head.blade.php
@@ -41,6 +41,8 @@
     <title>@yield('meta_title', config('app.name', 'Ash Redman - Developer'))</title>
 
     <!-- Scripts -->
+    <script src="{{ asset('js/manifest.js') }}" defer></script>
+    <script src="{{ asset('js/vendor.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,14 @@
 const mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
-//require("laravel-mix-purgecss");
 
 mix.sass("resources/sass/app.scss", "public/css").options({
     processCssUrls: false,
     postCss: [tailwindcss("tailwind.config.js"), require("autoprefixer")]
 });
 
-mix.js("resources/js/app.js", "public/js");
-
-// if (mix.inProduction()) {
-//     mix.purgeCss().version();
-// }
+mix.js("resources/js/app.js", "public/js").extract([
+    'axios',
+    'lodash',
+    'smooth-scroll',
+    'vanilla-lazyload'
+]);
