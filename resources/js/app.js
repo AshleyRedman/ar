636e2aa0c4add319d87d9ae3c4809742315db25a
Ashley Redman <ashredman@hotmail.com>
2020-05-24 13:54:49 +0100

    removed compiled assets from vc and added pretter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,28 @@
-import SmoothScroll from "smooth-scroll";
-import LazyLoad from "vanilla-lazyload";
+import SmoothScroll from 'smooth-scroll';
+import LazyLoad from 'vanilla-lazyload';
 
-const loader = document.querySelector(".loader");
-const tagName = document.querySelector(".tag-name");
-window.addEventListener("DOMContentLoaded", e => {
-
+const loader = document.querySelector('.loader');
+const tagName = document.querySelector('.tag-name');
+window.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
-        loader.classList.add("opacity-0");
+        loader.classList.add('opacity-0');
     }, 1000);
     if (tagName) {
         setTimeout(() => {
-            tagName.classList.add("w-auto", "pl-2", "pr-2");
+            tagName.classList.add('w-auto', 'pl-2', 'pr-2');
         }, 2000);
     }
 });
 
 const scroll = new SmoothScroll('a[href*="#');
 const ll = new LazyLoad({
-    elements_selector: ".lazy",
-    callback_loaded: el => {
-        el.classList.remove("blur");
-    }
+    elements_selector: '.lazy',
+    callback_loaded: (el) => {
+        el.classList.remove('blur');
+    },
 });
 
-window.onscroll = function() {
+window.onscroll = function () {
     scroller();
 };
 
@@ -34,17 +33,17 @@ function scroller() {
         document.documentElement.scrollHeight -
         document.documentElement.clientHeight;
     let scrolled = (winScroll / height) * 100;
-    document.getElementById("scrollBar").style.width = scrolled + "%";
+    document.getElementById('scrollBar').style.width = scrolled + '%';
 }
 
-const nav = document.querySelector(".mobile");
-const burger = document.querySelector(".burger");
-const navItem = document.querySelectorAll(".mobile-item");
+const nav = document.querySelector('.mobile');
+const burger = document.querySelector('.burger');
+const navItem = document.querySelectorAll('.mobile-item');
 
 function toggleMenu(e) {
-    nav.classList.toggle("translate-x-full");
+    nav.classList.toggle('translate-x-full');
 }
 
-burger.addEventListener("click", e => toggleMenu(e));
+burger.addEventListener('click', (e) => toggleMenu(e));
 
-navItem.forEach(i => i.addEventListener("click", e => toggleMenu(e)));
+navItem.forEach((i) => i.addEventListener('click', (e) => toggleMenu(e)));
